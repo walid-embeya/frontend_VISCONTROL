45576fef137de38b5e8ce05bc75e4ef1484d6fe9
Walid Embeya <walid.embeya@gmail.com>
2023-05-04 00:33:30 +0200

    crear y configurar store de pinia para invitados Fix #14

---
+++ frontend/src/stores/invitados.js
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia'
+import invitadosJson from '@/assets/invitados.json'
+
+const listaInvitados = invitadosJson._embedded.invitados
+
+export const invitadosStore = defineStore('invitados', {
+    state: () => ({         //// state: equiv a Data
+        invitados: listaInvitados
+    }),
+    //   getters: {         ////// getters: equiv a Computed
+    //     doubleCount: this.participantes.find(p => p.id == this.$route.params.id)
+    //   },
+    actions: {              ////// actions: equiv a Methods
+       getInvitadoPorMatricula(matricula) {
+           return this.invitados.find(i => i.matricula == matricula)
+       },
+       getInvitadoPorDNI(dni) {
+            return this.invitados.find(i => i.dni == dni)
+        },
+        getInvitadosPorEmpresa(empresa) {
+            return this.invitados.find(i => i.empresa == empresa)
+        }
+    },
+  })
