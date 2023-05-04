bf891a6e223bfa453da962d69fabf9acd28d61a9
Walid Embeya <walid.embeya@gmail.com>
2023-05-04 20:11:54 +0200

    Fix #17 crear un componente basico de invitado

---
+++ frontend/src/components/Invitado.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  props: [ 'invitado' ],  ///// aqui, la opcion "props" es un array of atributes string
+  data() {
+    return { }
+  }
+}
+</script>
+
+<template>
+    <!-- Este componente no necesita una capa <div></div> -->
+    
+    <tr>
+      <th scope="row">{{ invitado.matricula }}</th>
+      <td>{{ invitado.nombre }}</td>
+      <td>{{ invitado.apellidos }}</td>
+      <td>{{ invitado.empresa }}</td>
+     
+      <td><font-awesome-icon :icon="['fas', 'circle-check']" size="lg" class="option option1" /></td>
+      <td><font-awesome-icon :icon="['fas', 'pen-to-square']" size="lg" class="option" /></td>
+      <td><font-awesome-icon :icon="['fas', 'trash-can']" size="lg" class="option option3"/></td> 
+      <td> <router-link :to="`/invitados/${invitado.matricula}`">
+          <font-awesome-icon :icon="['fas', 'circle-info']" size="lg" style="color: #77767b;" /></router-link> 
+      </td>
+    </tr>
+</template>
+
+<style>
+  .option {
+    cursor: pointer;
+   }
+  
+  .option1 {
+    color: #26a269
+  }
+
+  .option3 {
+    color: red
+  }
+</style>
