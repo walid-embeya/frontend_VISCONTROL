e51a598e58bef4ed0da5f03fb1beb3a40a693118
Walid Embeya <walid.embeya@gmail.com>
2023-05-11 00:18:48 +0200

    Fix #19 creacion componente modelo

---
+++ frontend/src/components/Model.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    props: ['titulo']
+}
+
+</script>
+
+<template>
+     <!-- <div class="container alert alert-light"> -->
+    <div>
+        <h1 class="titulo">{{ titulo }}</h1>
+    
+        <slot></slot>
+    </div>
+</template>
