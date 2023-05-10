acd983bfdfb88b04c407ed8589be9a8d58bd0a95
Walid Embeya <walid.embeya@gmail.com>
2023-05-11 00:24:24 +0200

    Fix #18 creacion componente listado invitados

--- frontend/src/components/Invitado.vue
+++ frontend/src/components/Invitado.vue
@@ -3,9 +3,18 @@ export default {
   props: [ 'invitado' ],  ///// aqui, la opcion "props" es un array of atributes string
   data() {
     return { }
+  },
+  computed: {
+    autorizacion() {
+       return this.invitado.autorizacion.fechaInicio ==  null ? false : true;
+    }
+  },
+  created() {
+    // console.log(this.autorizacion)
   }
 }
 </script>
+
 
 <template>
     <!-- Este componente no necesita una capa <div></div> -->
@@ -13,22 +22,28 @@ export default {
     <tr>
       <th scope="row">{{ invitado.matricula }}</th>
       <td>{{ invitado.nombre }}</td>
-      <td>{{ invitado.apellidos }}</td>
+      <td>{{ invitado.apellidos }}</td> 
       <td>{{ invitado.empresa }}</td>
-     
+      <td v-if="autorizacion"><input type="checkbox" id="autorizado" name="autorizado" checked></td>
+      <td v-else><input type="checkbox" id="noAutorizado" name="noAutorizado"></td>
       <td><font-awesome-icon :icon="['fas', 'circle-check']" size="lg" class="option option1" /></td>
       <td><font-awesome-icon :icon="['fas', 'pen-to-square']" size="lg" class="option" /></td>
       <td><font-awesome-icon :icon="['fas', 'trash-can']" size="lg" class="option option3"/></td> 
-      <td> <router-link :to="`/invitados/${invitado.matricula}`">
-          <font-awesome-icon :icon="['fas', 'circle-info']" size="lg" style="color: #77767b;" /></router-link> 
+      <td>
+        <router-link :to="{ name: 'invitado', params: { identificador: invitado.matricula }}"><font-awesome-icon :icon="['fas', 'circle-info']" size="lg" style="color: #77767b;" /></router-link>
       </td>
+      
+      <!-- <td>      
+         <router-link :to="`/invitados/${invitado.matricula}`"><font-awesome-icon :icon="['fas', 'circle-info']" size="lg" style="color: #77767b;" /></router-link> 
+      </td> -->
     </tr>
 </template>
 
 <style>
+ 
   .option {
     cursor: pointer;
-   }
+  }
   
   .option1 {
     color: #26a269
---
+++ frontend/src/components/ListadoInvitados.vue
@@ -0,0 +1,53 @@
+<script>
+import Modelo from './Model.vue'
+import Invitado from './Invitado.vue'
+import { mapState } from 'pinia'
+import { invitadosStore } from '@/stores/invitados'
+
+
+export default {
+  components: { Invitado, Modelo },                   ///// registro local de los componentes "Modelo" y "Invitado"
+  data()  {
+    return {
+        //invitados: invitadosJson._embedded.invitados
+    }
+  },  
+  computed: {
+      ...mapState(invitadosStore, [ 'invitados' ])  
+  }
+}
+</script>
+
+<template>
+    <div>  
+        <Modelo titulo="LISTADO INVITADOS">
+          <router-link to="/nuevoinvitado" class="btn btn-secondary btn-lg my-2">
+              <font-awesome-icon :icon="['fas', 'circle-plus']" class="me-2"/>Nuevo Invitado</router-link>
+
+          <table class="table table-striped table-hover">
+              <thead class="alert alert-primary">
+                <tr>
+                  <th scope="col">Matricula</th>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Apellidos</th>
+                  <th scope="col">Empresa</th>
+                  <th scope="col" class="th-aut">Autorizado</th>
+                </tr>
+              </thead>
+              <tbody>
+                <Invitado v-for="invitado of invitados" :invitado="invitado" />      
+              </tbody>
+          </table>
+        </Modelo> 
+    </div>
+</template>
+
+
+
+<style>
+  /* .th-aut {
+    text-align: left;
+    align-self: left;
+    vertical-align: middle; 
+  }  */
+</style>
