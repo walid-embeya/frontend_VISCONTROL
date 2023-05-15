7312c827f2398f182ce278d28e2f2d5ac21063ec
Walid Embeya <walid.embeya@gmail.com>
2023-05-15 18:28:35 +0200

    añadir archivo utils

---
+++ frontend/src/utils/utils.js
@@ -0,0 +1,14 @@
+// export function dateToString(fecha) {
+//     return fecha.toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+//                  + ' a las ' + fecha.toLocaleTimeString('es-ES', { hour: "2-digit", minute: "2-digit" })
+// }
+
+export function timestampToFecha(fecha) {
+    let options = { year: 'numeric', month: 'long', day: 'numeric' }
+    return fecha.toLocaleDateString('es-ES', options)            
+}
+
+export function timestampToHora(fecha) {
+    let options = { hour: "2-digit", minute: "2-digit" }
+    return fecha.toLocaleTimeString('es-ES', options)
+}
